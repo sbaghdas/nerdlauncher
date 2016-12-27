bf8df78b0a616292e88003ac120ba9043fdca718
Suren Baghdasaryan <Suren@Surens-MacBook-Pro.local>
2016-12-26 17:16:11 -0800

    Get launcher activities and sort by name

--- app/src/main/java/com/example/suren/nerdlauncher/NerdLauncherFragment.java
+++ app/src/main/java/com/example/suren/nerdlauncher/NerdLauncherFragment.java
@@ -1,17 +1,27 @@
 package com.example.suren.nerdlauncher;
 
 import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
 
 public class NerdLauncherFragment extends Fragment {
+    private final static String LOG_TAG = NerdLauncherFragment.class.getSimpleName();
     private RecyclerView mRecyclerView;
 
     public NerdLauncherFragment() {
@@ -23,6 +33,24 @@ public class NerdLauncherFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_nerd_launcher, container, false);
         mRecyclerView = (RecyclerView)view.findViewById(R.id.fragment_nerd_launcher_recycler_view);
         mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        setupAdapter();
         return view;
+    }
+
+    private void setupAdapter() {
+        Intent startIntent = new Intent(Intent.ACTION_MAIN);
+        startIntent.addCategory(Intent.CATEGORY_LAUNCHER);
+        PackageManager pm = getActivity().getPackageManager();
+        List<ResolveInfo> activities = pm.queryIntentActivities(startIntent, 0);
+        Collections.sort(activities, new Comparator<ResolveInfo>() {
+            @Override
+            public int compare(ResolveInfo r1, ResolveInfo r2) {
+                PackageManager pm = getActivity().getPackageManager();
+                String label1 = r1.loadLabel(pm).toString();
+                String label2 = r2.loadLabel(pm).toString();
+                return label1.compareTo(label2);
+            }
+        });
+        Log.i(LOG_TAG, "Number of activities found: " + activities.size());
     }
 }
